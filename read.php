4b744cf7877aba19f57469ac50a8f5fd8aa69fc2
Daryll Culas <129874425+DaryllCulas@users.noreply.github.com>
2023-04-06 17:56:37 +0800

    v2 commit
    Some file has been modified and done
    
    # Need to make update and delete process file

--- read.php
+++ read.php
@@ -1,7 +1,8 @@
 <?php 
-    require('./database.php');
+    require('./database.php'); # strict include of this file in order to be accessible of its function
+
+    $queryAccounts = "SELECT * FROM accounts"; # Query of Table in Database
+    $sqlAccounts = mysqli_query($connection, $queryAccounts); # Combination of Query of connection and database
 
 
-
-
-?>
+ ?>
